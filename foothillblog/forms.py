9210800751be83b2e6470b3439818e44b7bb0621
Christian <cgao273@gmail.com>
2018-12-02 20:44:04 +0300

    fourth commit

--- foothillblog/forms.py
+++ foothillblog/forms.py
@@ -1,8 +1,8 @@
 from foothillblog.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo,
-ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo,\
+                                ValidationError
 
 
 class RegistrationForm(FlaskForm):
