dd32dba2d14fc3040e466e73c77a7a8fa339f23f
Christian <cgao273@gmail.com>
2018-12-03 16:02:08 +0300

    fifth commit

--- foothillblog/routes.py
+++ foothillblog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from foothillblog import app, db, bcrypt
-from foothillblog.forms import RegistrationForm, LoginForm
+from foothillblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from foothillblog.models import User, PostSentiments, RealTweets
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -75,7 +75,20 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
